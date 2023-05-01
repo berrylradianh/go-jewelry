84f2ee6bae2f2558bb41abc3effb7d5733a8bd87
berrylradianh <berrylhamesha@gmail.com>
2023-05-01 16:15:46 +0700

    editing main.go, go mod & go sum. And add gitignore

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"log"
+	"os"
+
+	mws "github.com/berrylradianh/go-jewelry/middlewares"
+	rts "github.com/berrylradianh/go-jewelry/routes"
 
 	"github.com/joho/godotenv"
 )
@@ -12,5 +16,7 @@ func init() {
 
 func main() {
 	log.Println("Starting the application...")
-
+	e := rts.InitRoutes()
+	mws.LogMiddleware(e)
+	e.Logger.Fatal(e.Start(os.Getenv("APP_PORT")))
 }
